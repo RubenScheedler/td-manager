5a0bb428a91f778a0d4c58ffbd5b834880dca727
Ruben <rubenscheedler@gmail.com>
2023-12-15 12:37:28 +0100

    Add Spring Initialzr code

---
+++ src/main/kotlin/dev/hashnode/rubenscheedler/tdmanager/TdmanagerApplication.kt
@@ -0,0 +1,11 @@
+package dev.hashnode.rubenscheedler.tdmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TdmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TdmanagerApplication>(*args)
+}
---
+++ src/test/kotlin/dev/hashnode/rubenscheedler/tdmanager/TdmanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hashnode.rubenscheedler.tdmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TdmanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
